3ac2d763aabfb550373d98abdc3f1b4561db89fe
mhatw <contactomhatw@gmail.com>
2022-03-03 17:52:28 -0500

    start project :D

--- clin_boards.rb
+++ clin_boards.rb
@@ -1,6 +1,14 @@
+
+require "json"
+file = File.read('store.json')
+
+data_hash = JSON.parse(file)
+print data_hash
+
 class ClinBoards
   def initialize
     # Complete this
+
   end
 
   def start
@@ -9,5 +17,3 @@ class ClinBoards
 end
 
 # get the command-line arguments if neccesary
-app = ClinBoards.new
-app.start
